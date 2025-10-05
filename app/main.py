410d0a366e9a3833249fbd272dad23b787c46d6d
nepalankit <nepalankit16@gmail.com>
2025-10-05 13:54:23 +0545

    ADD:user schemas and routes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI,status,HTTPException,Response,Depends
 from fastapi import Body
-from pydantic import BaseModel
 from typing import Optional, Dict, List
 from random import randrange
 from sqlalchemy.orm import Session
@@ -9,9 +8,17 @@ import time
 from psycopg2.extras import RealDictCursor
 import os
 from dotenv import load_dotenv
-from .import models
+from .import models,utils
 from .database import engine,get_db
-from .schemas import PostBase,Post,PostCreate,UserCreate,UserOut
+
+from .routers import post,user
+
+
+
+
+
+
+
 
 models.Base.metadata.create_all(bind=engine)
 load_dotenv()
@@ -38,9 +45,10 @@ while True:
         time.sleep(2)
     
     
+app.include_router(post.router)
+app.include_router(user.router)
 
-my_posts=[{"title":"title of post 1", "content":"content of post 1","id":1},
-          {"title":"title of post 2", "content":"content of post 2","id":2}]
+
 
 @app.get('/') #decorator makes api endpoint
 async def root():
@@ -52,95 +60,5 @@ async def root():
 #         return {"data":posts}
 
 
-@app.get('/posts',response_model=List[Post])
-async def get_posts(db:Session=Depends(get_db)):
-    # cur.execute(""" SELECT * FROM posts""")
-    # posts=cur.fetchall()
-        posts= db.query(models.Post).all() #.all() runs sql query
-        return posts
 
 
-@app.post('/posts',status_code=status.HTTP_201_CREATED,response_model=Post )
-def create_posts(post:PostCreate,db:Session=Depends(get_db)):
-    # cur.execute("""INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING * """,(post.title,post.content,post.published))
-    # new_post=cur.fetchone()
-    # conn.commit()
-    
-   
-    # new_post=models.Post(title=post.title,content=post.content,published=post.published)
-     #convereting this line to dictionary is really handy.Does the same thing as above
-    new_post=models.Post(**post.dict())
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post) # just like returning *
-    return new_post
-
-
-
-
-
-#single posts
-# def find_post(id):
-#     for p in my_posts:
-#         if p['id']==id:
-#             return p
-
-@app.get('/posts/{id}',response_model=Post)
-def get_post(id:int,db:Session=Depends(get_db)):
-    # cur.execute(""" SELECT * FROM posts where id=%s""",(str(id),))
-    # single_post=cur.fetchone()
-    # print(single_post)
-    single_post=db.query(models.Post).filter(models.Post.id==id).first()
-    if not single_post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id:{id} was not found")
-
-
-    return single_post
-
-
-#delete post
-
-# def find_index_post(id):
-#     for i,p in enumerate(my_posts):
-#         if p['id']==id:
-#             return i
-
-@app.delete('/posts/{id}',status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id:int,db:Session=Depends(get_db)):
-    # cur.execute("""DELETE FROM posts WHERE id=%s RETURNING *""",(str(id),))
-    # deleted_post=cur.fetchone()
-    # conn.commit()
-    posts=db.query(models.Post).filter(models.Post.id==id)
-    
-    if posts.first()==None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
-    
-    posts.delete(synchronize_session=False)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-    
-@app.put('/posts/{id}',response_model=PostCreate)
-def update_post(id:int,updated_post:PostCreate,db:Session=Depends(get_db)):
-    # cur.execute("""UPDATE posts SET title=%s,content=%s,published=%s where id=%s RETURNING * """,(post.title,post.content,post.published,str(id)))
-    # updated_post=cur.fetchone()
-    # conn.commit()
-    post_query=db.query(models.Post).filter(models.Post.id==id)
-
-    if post_query.first()==None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
-    
-    post_query.update(updated_post.dict(),synchronize_session=False)
-    db.commit()
-
-    return post_query.first()
-
-
-@app.post('/users',status_code=status.HTTP_201_CREATED,response_model=UserOut)
-def create_user(user:UserCreate,db:Session=Depends(get_db)):
-    new_user=models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
---
+++ app/routers/post.py
@@ -0,0 +1,106 @@
+from fastapi import FastAPI,status,HTTPException,Response,Depends,APIRouter
+from fastapi import Body
+from pydantic import BaseModel
+from typing import Optional, Dict, List
+from random import randrange
+from sqlalchemy.orm import Session
+import psycopg2
+import time
+from psycopg2.extras import RealDictCursor
+import os
+from dotenv import load_dotenv
+from ..import models,utils
+from ..database import engine,get_db
+from ..schemas import Post,PostCreate
+from fastapi.security import OAuth2PasswordRequestForm
+
+
+router=APIRouter(
+    prefix="/posts",
+    tags=["Posts"]
+)
+
+
+@router.get('/',response_model=List[Post])
+async def get_posts(db:Session=Depends(get_db)):
+    # cur.execute(""" SELECT * FROM posts""")
+    # posts=cur.fetchall()
+        posts= db.query(models.Post).all() #.all() runs sql query
+        return posts
+
+
+@router.post('/',status_code=status.HTTP_201_CREATED,response_model=Post )
+def create_posts(post:PostCreate,db:Session=Depends(get_db)):
+    # cur.execute("""INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING * """,(post.title,post.content,post.published))
+    # new_post=cur.fetchone()
+    # conn.commit()
+    
+   
+    # new_post=models.Post(title=post.title,content=post.content,published=post.published)
+     #convereting this line to dictionary is really handy.Does the same thing as above
+    new_post=models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post) # just like returning *
+    return new_post
+
+
+
+
+
+#single posts
+# def find_post(id):
+#     for p in my_posts:
+#         if p['id']==id:
+#             return p
+
+@router.get('/{id}',response_model=Post)
+def get_post(id:int,db:Session=Depends(get_db)):
+    # cur.execute(""" SELECT * FROM posts where id=%s""",(str(id),))
+    # single_post=cur.fetchone()
+    # print(single_post)
+    single_post=db.query(models.Post).filter(models.Post.id==id).first()
+    if not single_post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id:{id} was not found")
+
+
+    return single_post
+
+
+#delete post
+
+# def find_index_post(id):
+#     for i,p in enumerate(my_posts):
+#         if p['id']==id:
+#             return i
+
+@router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id:int,db:Session=Depends(get_db)):
+    # cur.execute("""DELETE FROM posts WHERE id=%s RETURNING *""",(str(id),))
+    # deleted_post=cur.fetchone()
+    # conn.commit()
+    posts=db.query(models.Post).filter(models.Post.id==id)
+    
+    if posts.first()==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
+    
+    posts.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    
+@router.put('/{id}',response_model=PostCreate)
+def update_post(id:int,updated_post:PostCreate,db:Session=Depends(get_db)):
+    # cur.execute("""UPDATE posts SET title=%s,content=%s,published=%s where id=%s RETURNING * """,(post.title,post.content,post.published,str(id)))
+    # updated_post=cur.fetchone()
+    # conn.commit()
+    post_query=db.query(models.Post).filter(models.Post.id==id)
+
+    if post_query.first()==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
+    
+    post_query.update(updated_post.dict(),synchronize_session=False)
+    db.commit()
+
+    return post_query.first()
