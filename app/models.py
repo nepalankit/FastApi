6bbb7fb7f2c3f93f86bf426b0685087df1b21edb
nepalankit <nepalankit16@gmail.com>
2025-10-24 20:05:56 +0545

    added prod files

--- app/models.py
+++ app/models.py
@@ -30,3 +30,4 @@ class Like(Base):
     __tablename__="likes"
     user_id=Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),primary_key=True)
     post_id=Column(Integer,ForeignKey("posts.id",ondelete="CASCADE"),primary_key=True)
+    
